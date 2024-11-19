ab42e175241ed7377be439b612e6d181125ad74c
R3aperCitizen <justr3aper3@gmail.com>
2024-11-19 16:47:59 +0100

    setup

--- smartbin/smartbin.ino
+++ smartbin/smartbin.ino
@@ -4,17 +4,32 @@
 #include <LowPower.h>
 #include <PinChangeInterrupt.h>
 
+//digital pin
 #define PIR 2
 #define SERVO 3
-#define SONAR_SEND 4
-#define SONAR_RECEIVE 5
+#define SONAR_TRIG 4
+#define SONAR_ECHO 5
 #define OPEN_BUTTON 6
 #define CLOSE_BUTTON 7
 #define GREEN_LED 8
 #define RED_LED 9
 
-void setup() {
+//analog pin
+#define TEMP_SENSOR A3
 
+Servo door;
+
+void setup() {
+    Serial.begin(9600);
+    door.attach(SERVO);  // attaches the servo on pin SERVO to the servo object
+    
+    pinMode(GREEN_LED, OUTPUT);
+    pinMode(SONAR_TRIG, OUTPUT);
+    pinMode(SONAR_ECHO, INPUT);
+    pinMode(OPEN_BUTTON, INPUT_PULLUP);
+    pinMode(CLOSE_BUTTON, INPUT_PULLUP);
+    pinMode(GREEN_LED, OUTPUT);
+    pinMode(RED_LED, OUTPUT);
 }
 
 void loop() {
