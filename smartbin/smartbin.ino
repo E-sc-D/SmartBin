75b73167beb9a5049bbe7949e59749e9e38c7a22
R3aperCitizen <justr3aper3@gmail.com>
2024-11-19 16:40:51 +0100

    Added pin defines

--- smartbin/smartbin.ino
+++ smartbin/smartbin.ino
@@ -4,6 +4,15 @@
 #include <LowPower.h>
 #include <PinChangeInterrupt.h>
 
+#define PIR 2
+#define SERVO 3
+#define SONAR_SEND 4
+#define SONAR_RECEIVE 5
+#define OPEN_BUTTON 6
+#define CLOSE_BUTTON 7
+#define GREEN_LED 8
+#define RED_LED 9
+
 void setup() {
 
 }
