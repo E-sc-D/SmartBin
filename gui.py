e2089d200ba38cbfedade4ce08112b57a3f4b0a8
R3aperCitizen <justr3aper3@gmail.com>
2024-12-03 15:29:49 +0100

    graphic fix

--- gui.py
+++ gui.py
@@ -16,11 +16,11 @@ def update_distance():
                 distanza = arduino.readline().decode('utf-8').strip()   #legge il primo numero dal monitor seriale e lo mette nella variabile distanza (è gia convertita in percentuale rispetto alla distanza massima)
                 temperatura = arduino.readline().decode('utf-8').strip()    #legge il secondo numero dal monitor seriale e lo mette nella variavile temperatura
                 if distanza.isdigit():
-                    distance_label.config(text=f"Livello bidone: {distanza} %")         
-                    temperature_label.config(text=f"Temperatura: {temperatura} °")
+                    distance_label.config(text=f"Container Level: {distanza} %")         
+                    temperature_label.config(text=f"Temperature: {temperatura} °")
                     progress_var.set(distanza)
     except Exception as e:
-        print(f"Errore: {e}")
+        print(f"Error: {e}")
 
 
 
@@ -49,7 +49,7 @@ label1 = Label(root, image=bg)
 label1.place(x=0, y=0, relwidth=1, relheight=1)
 
 # Label per visualizzare la distanza
-distance_label = tk.Label(root, text="Livello bidone: -- %", fg="yellow3", font=("Sans", 16))
+distance_label = tk.Label(root, text="Container Level: -- %", fg="yellow3", font=("Rockwell", 16))
 distance_label.pack(pady=(20, 0))
 distance_label.configure(bg="gray12")
 
@@ -59,16 +59,16 @@ progress_bar = ttk.Progressbar(root, variable=progress_var, maximum=100)
 progress_bar.pack(padx=100, pady=10, fill="x")
 
 # Label per visualizzare la temperatura
-temperature_label = tk.Label(root, text="Temperatura: -- °", fg="yellow3", font=("Sans", 16))
+temperature_label = tk.Label(root, text="Temperature: -- °", fg="yellow3", font=("Rockwell", 16))
 temperature_label.pack(padx=80,pady=0)
 temperature_label.configure(bg="gray12")
 
 # Bottone per svuotare il bidone
-empty_button = tk.Button(root,bg='gray12', text="Empty the container", fg="yellow3", command=empty_bin, font=("Sans", 14))
+empty_button = tk.Button(root,bg='gray12', text="Empty the Container", fg="yellow3", command=empty_bin, font=("Rockwell", 14))
 empty_button.pack(pady=(150, 0))
 
 # Bottone per resettare la macchina
-reset_button = tk.Button(root,bg='gray12', text="RESTORE", fg="yellow3", command=reset_bin, font=("Sans", 14))
+reset_button = tk.Button(root,bg='gray12', text="RESTORE", fg="yellow3", command=reset_bin, font=("Rockwell", 14))
 reset_button.pack(pady=10)
 
 # Thread per aggiornare la distanza in modo asincrono
