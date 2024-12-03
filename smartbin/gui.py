a17a097ccae3bd4e0a66764eec593fb2c16a936c
Giuseppe <giuseppeargentiere@outlook.it>
2024-12-03 12:11:27 +0100

    Changed gui color and adjusted size

--- smartbin/gui.py
+++ smartbin/gui.py
@@ -4,20 +4,23 @@ from tkinter import ttk
 import serial
 import threading
 
-arduino = serial.Serial(port='COM3', baudrate=9600, timeout=1)
+arduino = serial.Serial(port='/dev/ttyACM0', baudrate=9600, timeout=1)
 
 def update_distance():
     """Aggiorna la distanza letta dal sonar e dal sensore"""
     try:
         while True:
             if arduino.in_waiting > 0:
-                distanza = arduino.readline().decode('utf-8').strip() 
-                temperatura = arduino.readline().decode('utf-8').strip()
+                distanza = arduino.readline().decode('utf-8').strip()   #legge il primo numero dal monitor seriale e lo mette nella variabile distanza (è gia convertita in percentuale rispetto alla distanza massima)
+                temperatura = arduino.readline().decode('utf-8').strip()    #legge il secondo numero dal monitor seriale e lo mette nella variavile temperatura
                 if distanza.isdigit():
-                    distance_label.config(text=f"Livello bidone: {distanza} %")
+                    distance_label.config(text=f"Livello bidone: {distanza} %")         
                     temperature_label.config(text=f"Temperatura: {temperatura} °")
+                    progress_var.set(distanza)
     except Exception as e:
         print(f"Errore: {e}")
+
+
 
 def empty_bin():
     try:
@@ -34,21 +37,32 @@ def reset_bin():
 # Crea la finestra principale
 root = tk.Tk()
 root.title("Smart bin")
+root.geometry("400x500")
+root.configure(bg='DarkOrchid2')
 
 # Label per visualizzare la distanza
-distance_label = tk.Label(root, text="Livello bidone: -- %", font=("Helvetica", 16))
+distance_label = tk.Label(root, text="Livello bidone: -- %", font=("Vivaldi", 16))
 distance_label.pack(pady=20)
+distance_label.configure(bg='green3')
+
+#Label per la barra di progressone
+progress_var = tk.DoubleVar()
+progress_bar = ttk.Progressbar(root, variable=progress_var, maximum=100)
+progress_bar.pack(padx=100, pady=20, fill="x")
+
+
 
 # Label per visualizzare la temperatura
-temperature_label = tk.Label(root, text="Temperatura: -- °", font=("Helvetica", 16))
-temperature_label.pack(pady=20)
+temperature_label = tk.Label(root, text="Temperatura: -- °", font=("Vivaldi", 16))
+temperature_label.pack(padx=80,pady=20)
+temperature_label.configure(bg='green3')
 
 # Bottone per svuotare il bidone
-empty_button = tk.Button(root, text="Empty the container", command=empty_bin, font=("Helvetica", 14))
+empty_button = tk.Button(root,bg='green3', text="Empty the container", command=empty_bin, font=("Vivaldi", 14))
 empty_button.pack(pady=20)
 
 # Bottone per resettare la macchina
-reset_button = tk.Button(root, text="RESTORE", command=reset_bin, font=("Helvetica", 14))
+reset_button = tk.Button(root,bg='green3', text="RESTORE", command=reset_bin, font=("Vivaldi", 14))
 reset_button.pack(pady=20)
 
 # Thread per aggiornare la distanza in modo asincrono
